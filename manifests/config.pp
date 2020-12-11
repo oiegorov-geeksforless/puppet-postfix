97df604f77f6692455f8bc99c9ca56880b377f28
oiegorov-geeksforless <73962065+oiegorov-geeksforless@users.noreply.github.com>
2020-12-11 14:19:01 -0500

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -33,36 +33,37 @@ define postfix::config (
 ) {
 
   if ($ensure == 'present') {
-    assert_type(Pattern[/^.+$/], $value) |$e, $a| {
-      fail "value for parameter: ${title} can not be empty if ensure = present"
-    }
+    if ( defined($value) ) {
+      assert_type(Pattern[/^.+$/], $value) |$e, $a| {
+       fail "value for parameter: ${title} can not be empty if ensure = present"
+         if (!defined(Class['postfix'])) {
+          fail 'You must define class postfix before using postfix::config!'
+         }
+
+      case $ensure {
+        'present': {
+          $changes = "set ${name} '${value}'"
+        }
+        'absent': {
+          $changes = "rm ${name}"
+        }
+        'blank': {
+          $changes = "clear ${name}"
+        }
+        default: {
+          fail "Unknown value for ensure '${ensure}'"
+        }
+      }
+
+      augeas { "manage postfix '${title}'":
+        incl    => '/etc/postfix/main.cf',
+        lens    => 'Postfix_Main.lns',
+        changes => $changes,
+        require => File['/etc/postfix/main.cf'],
+      }
+
+      Postfix::Config[$title] ~> Class['postfix::service']
+     }
   }
 
-  if (!defined(Class['postfix'])) {
-    fail 'You must define class postfix before using postfix::config!'
-  }
-
-  case $ensure {
-    'present': {
-      $changes = "set ${name} '${value}'"
-    }
-    'absent': {
-      $changes = "rm ${name}"
-    }
-    'blank': {
-      $changes = "clear ${name}"
-    }
-    default: {
-      fail "Unknown value for ensure '${ensure}'"
-    }
-  }
-
-  augeas { "manage postfix '${title}'":
-    incl    => '/etc/postfix/main.cf',
-    lens    => 'Postfix_Main.lns',
-    changes => $changes,
-    require => File['/etc/postfix/main.cf'],
-  }
-
-  Postfix::Config[$title] ~> Class['postfix::service']
 }
