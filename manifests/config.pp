74fecd55109f10291fe64f924bb4e5c0eab59946
oiegorov-geeksforless <73962065+oiegorov-geeksforless@users.noreply.github.com>
2020-12-14 09:08:14 -0500

    Update config.pp
    Added notes.

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,6 +27,17 @@
 #     ensure => 'blank',
 #   }
 #
+# === Notes
+# Worth mentioning that if an $ensure is 'present', however $value was not provided -
+# it is implied that a user did not make a mistake, rather made a stub reservation
+# for this $title. For this case, making a defined type to return without failing
+# Use case: multi-server type deployments, which use postfix puppet module. They
+# normally yes a wrapper around it (a.k.a profile). This is beneficial to have this
+# wrappe configured with a union of config options used for all server types. Then
+# use hiera to configure necessary options for each server type individually. Currently
+# such an approach fails, due to assert_type check. Without this functionality it would be
+# required to maintain X number of profiles - 1 per server type.
+# 
 define postfix::config (
   Optional[String]                   $value  = undef,
   Enum['present', 'absent', 'blank'] $ensure = 'present',
